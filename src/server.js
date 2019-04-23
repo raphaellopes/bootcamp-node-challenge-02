5c7602682e774f6ce9089dde0825db581fc0a095
Raphael Lopes <ph.campanario07@gmail.com>
2019-04-23 10:45:37 -0300

    Create project structure

---
+++ src/server.js
@@ -0,0 +1,37 @@
+// vendors
+const express = require('express');
+const nunjucks = require('nunjucks');
+const path = require('path');
+
+// local
+const routes = require('./routes');
+
+class App {
+  constructor() {
+    this.express = express();
+    this.isDev = process.env.NODE_ENV;
+    this.middlewares();
+    this.views();
+    this.routes();
+  }
+
+  middlewares() {
+    this.express.use(express.urlencoded({ extended: false }));
+  }
+
+  views() {
+    nunjucks.configure(path.resolve(__dirname, 'app', 'views'), {
+      watch: this.isDev,
+      express: this.express,
+      autoscape: true,
+    });
+
+    this.express.set('view engine', 'njk');
+  }
+
+  routes() {
+    this.express.use(routes);
+  }
+}
+
+module.exports = new App().express;
