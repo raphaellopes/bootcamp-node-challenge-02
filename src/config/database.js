fb45dea87fba1b8a24412ad0ee26ff4e5e54c9c7
Raphael Lopes <ph.campanario07@gmail.com>
2019-04-23 12:21:09 -0300

    Database config

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   host: '127.0.0.1',
   username: 'docker',
   password: 'docker',
-  database: 'gonodemodulo2',
+  database: 'gonode_modulo2',
   operatorAliases: false,
   define: {
     timestamp: true,
