97a41137986b10ec0760b92e51e4f0a1b29ab5ee
Raphael Lopes <ph.campanario07@gmail.com>
2019-04-24 13:08:35 -0300

    Save service

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,10 +1,29 @@
-const { User } = require('../models');
+const { User, Appointment } = require('../models');
 
 class AppointmentController {
   async create (req, res) {
     const provider = await User.findByPk(req.params.provider);
 
     return res.render('appointments/create', { provider });
+  }
+
+  async store (req, res) {
+    try {
+      const { id } = req.session.user;
+      const { provider } = req.params;
+      const { date } = req.body;
+      const data = {
+        user_id: id,
+        provider_id: provider,
+        date
+      };
+
+      await Appointment.create(data);
+
+      return res.redirect('/app/dashboard');
+    } catch (error) {
+      console.log({ error });
+    }
   }
 };
 
